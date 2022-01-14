84ca13fda3199a82cde15fe5dc560908a0a436f6
macarl08 <macarl@outlook.com>
2022-01-14 17:55:39 -0500

    template

---
+++ example.py
@@ -0,0 +1,4 @@
+#example.py
+
+
+print("test")
---
+++ main_attempt1.py
@@ -0,0 +1,16 @@
+# UTEK 2022 Programming
+# Team x 
+# main.py
+
+# Contact: carlk.ma@mail.utoronto.ca
+# NOTE: Attempt 1 - TEST SUBMISSION (TO BE UPDATED)
+
+
+f = open("1.in", "r")
+data = f.readlines()
+f.close()
+
+
+
+g = open("1.out", "w")
+g.close()
